b7045257059f14bb4fccae8c2f6500e5d1597dbe
Nejikugi <63852665+scrwnl@users.noreply.github.com>
2024-03-29 04:45:30 +0900

    Create Files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿var typeNames = new List<string> {
+    "System.Boolean",
+    "System.Byte",
+    "System.SByte",
+    "System.Char",
+    "System.Decimal",
+    "System.Double",
+    "System.Single",
+    "System.Int32",
+    "System.UInt32",
+    "System.IntPtr",
+    "System.UIntPtr",
+    "System.Int64",
+    "System.UInt64",
+    "System.Int16",
+    "System.UInt16",
+    "System.Object",
+    "System.String"
+};
+
+foreach (string typeName in typeNames) {
+    Console.WriteLine($"Type: {typeName}, DefaultValue: {Activator.CreateInstance(Type.GetType(typeName))}");
+};
+
